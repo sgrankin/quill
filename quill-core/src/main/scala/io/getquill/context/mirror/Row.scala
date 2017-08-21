cca518951cbfe8446e4425ea84a6bf5a5ff5e7c6
Sergey Grankin <sgrankin@iown1.net>
2017-08-21 10:37:44 -0400

    context.mirror.Row
    --
    io.getquill.QueryBenchmark
    
    before:
    Parameters(size -> 1): 0.153075
    Parameters(size -> 10): 0.260952
    Parameters(size -> 100): 1.168971
    Parameters(size -> 1000): 10.613932
    Parameters(size -> 10000): 488.601148
    
    after:
    Parameters(size -> 1): 0.146182
    Parameters(size -> 10): 0.649823
    Parameters(size -> 100): 1.300798
    Parameters(size -> 1000): 9.989556
    Parameters(size -> 10000): 203.262953

--- quill-core/src/main/scala/io/getquill/context/mirror/Row.scala
+++ quill-core/src/main/scala/io/getquill/context/mirror/Row.scala
@@ -4,11 +4,15 @@ import scala.reflect.ClassTag
 
 import io.getquill.util.Messages.fail
 
-case class Row(data: Any*) {
-  def add(value: Any) = Row((data :+ value): _*)
+case class Row private[mirror] (data: Vector[Any]) {
+  def add(value: Any) = Row(data :+ value)
   def apply[T](index: Int)(implicit t: ClassTag[T]) =
     data(index) match {
       case v: T  => v
       case other => fail(s"Invalid column type. Expected '${t.runtimeClass}', but got '$other'")
     }
+}
+
+object Row {
+  def apply(xs: Any*): Row = new Row(Vector(xs: _*))
 }
